e2d4f9eafaab2a8addca831882b2d9fb80cd8438
Guy David <gaidavid1@gmail.com>
2023-10-26 00:51:31 +0300

    Update database.js
    applying useNewUrlParser and useUnifiedTopology options for more reliable connection behavior.

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require(`mongoose`);
 
 const connect = async (username,password,host)=>{
     const uri =`mongodb+srv://${username}:${password}@${host}?retryWrites=true&w=majority`;
-    await mongoose.connect(uri);
+    await mongoose.connect(uri ,{ useNewUrlParser: true, useUnifiedTopology: true });
     await mongoose.set('strictQuery', false)
     console.log(`connected to database`);
 };
